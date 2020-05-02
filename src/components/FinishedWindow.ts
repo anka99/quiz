7a6dacf2085b5c541a84e54b61bdc1acc6cae167
Anq <ak406173@students.mimuw.edu.pl>
2020-05-02 14:16:13 +0200

    Mobile view

--- src/components/FinishedWindow.ts
+++ src/components/FinishedWindow.ts
@@ -3,7 +3,8 @@ import { numberToTime } from "../tools/timeTools.js";
 class FinishedWindow {
   static render(time: number) {
     let el = document.createElement("div");
-    let text = document.createElement("p");
+    el.setAttribute("class", "loooser");
+    let text = document.createElement("h1");
     text.textContent = "congratulations 2/10 your time " + numberToTime(time);
     el.appendChild(text);
     return el;
